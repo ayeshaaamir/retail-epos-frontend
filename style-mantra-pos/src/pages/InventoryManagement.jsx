03870062e6fc29e2b4c268b2f1fab247c9c03cf1
ayeshaaamir <ayeshaaamir38@gmail.com>
2025-03-01 21:41:44 +0000

    size options added..

--- style-mantra-pos/src/pages/InventoryManagement.jsx
+++ style-mantra-pos/src/pages/InventoryManagement.jsx
@@ -88,6 +88,16 @@ const InventoryManagement = () => {
     { label: "M", value: "M" },
     { label: "S", value: "S" },
     { label: "XS", value: "XS" },
+    { label: "10", value: "10" },
+    { label: "12", value: "12" },
+    { label: "14", value: "14" },
+    { label: "16", value: "16" },
+    { label: "18", value: "18" },
+    { label: "20", value: "20" },
+    { label: "22", value: "22" },
+    { label: "24", value: "24" },
+    { label: "26", value: "26" },
+    { label: "28", value: "28" },
   ];
 
   return (
